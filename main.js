c38348be0eb7f191b5fb6bb6bc7e2e2aa286dafa
aberrantian <aberrantian@gmail.com>
2022-10-07 17:34:59 +0100

    create getComputerChoice function

---
+++ main.js
@@ -0,0 +1,28 @@
+/*
+Randomly pick from "Rock", "Paper", or "Scissors"
+Prompt user for their choice
+Compare answers
+Tell user if they won/lost/tied
+Ask to play again
+If yes
+    play again
+else
+    end
+*/
+
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+
+    if (choice === 0) {
+        return "Rock"
+    }
+    else if (choice === 1) {
+        return "Paper"
+    }
+    else if (choice === 2) {
+        return "Scissors"
+    }
+}
+
+console.log(getComputerChoice())
+
