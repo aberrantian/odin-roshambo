c764895a303dd2c81df3e92d24eb6be945b14d1b
aberrantian <aberrantian@gmail.com>
2022-10-27 06:40:21 +0100

    setup points and games counter

--- main.js
+++ main.js
@@ -12,28 +12,41 @@ function getComputerChoice() {
     }
 }
 
+let points = 0;
+let gamesPlayed = 0;
+
 function playRoshambo(userChoice) {
     const computerChoice = getComputerChoice();
 
     if (computerChoice === userChoice) {
+        gamesPlayed++;
         return `Tie! You both chose ${userChoice}`;
     };
 
     if (userChoice === 'rock' && computerChoice === 'paper') {
+        gamesPlayed++;
         return 'You lost! Paper beats rock';
     } else if (userChoice === 'rock' && computerChoice === 'scissors') {
+        gamesPlayed++;
+        points++;
         return 'You won! Rock beats scissors';
     };
 
     if (userChoice === 'paper' && computerChoice === 'rock') {
+        gamesPlayed++;
+        points++;
         return 'You won! Paper beats rock';
     } else if (userChoice === 'paper' && computerChoice === 'scissors') {
+        gamesPlayed++;
         return 'You lost! Scissors beats paper';
     };
 
     if (userChoice === 'scissors' && computerChoice === 'rock') {
+        gamesPlayed++;
         return 'You lost! Rock beats scissors';
     } else if (userChoice === 'scissors' && computerChoice === 'paper') {
+        gamesPlayed++;
+        points++;
         return 'You won! Scissors beats paper';
     };
 }
